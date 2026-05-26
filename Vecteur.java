d66811942e9fc0e8f03c217905e4671ff0a2f59d
Yassine Harrach <yassinocegb@gmail.com>
2026-05-26 10:29:09 +0200

    créationDeVecteur.java

---
+++ Vecteur.java
@@ -0,0 +1,27 @@
+package Projet;
+
+public class Vecteur {
+	
+	private int[] vecteur;
+	
+	public  Vecteur(int dimension) {
+		this.vecteur = new int[dimension];
+	}
+	
+	public  Vecteur(int[] valeur) {
+		this.vecteur = valeur;
+	}
+
+	public void centraliser(Vecteur visageMoyen) {
+		
+		int[] tabmoy = visageMoyen.getVecteur();
+		
+		for(int i = 0; i< this.vecteur.length; i++) {
+			this.vecteur[i] -= tabmoy[i];
+		}
+	}
+
+	private int[] getVecteur() {
+		return null;
+	}
+}
