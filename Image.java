c95344b799a4eb98a6894d035cd87a15ff86d029
clementPicard64 <clement.picard64000@gmail.com>
2026-05-26 16:48:24 +0200

    Modification Image

--- Image.java
+++ Image.java
@@ -7,28 +7,44 @@ import javax.imageio.ImageIO;
 
 public class Image {
 	
-	private String id; //Pas encore fait (initialisation)
-	private BufferedImage img;
+	private String id;
+	private BufferedImage img; //img stockée avec BufferedImage
 	
 	//private boolean estDeReference;
-	private int largeurImage;
-	private int hauteurImage;
-	private String cheminImage;
-	private double[][] matriceImage;
-	private Vecteur vecteurImage;
+	private int largeurImage; //Largeur de l'image 
+	private int hauteurImage; //Longueur de l'image 
+	private String cheminImage; //Chemin de l'image
+	private double[][] matriceImage; //Matrice de l'image (tableau 2D)
+	private Vecteur vecteurImage; //Vecteur colonne de l'image de type de la classe Vecteur
 	
-	public Image(String Chemin) throws IOException {
-		this.cheminImage = Chemin;
+	
+	/**
+	 * 
+	 * @author Clément
+	 * @param Chemin : chemin de l'image 
+	 * @throws IOException (ajout de l'exception plus tard)
+	 */
+	public Image(String id,String CheminPersonne) throws IOException {
+		this.id = id;
+		this.cheminImage = CheminPersonne+ "/" + id;
 		this.img = ImageIO.read(new File(this.cheminImage));
 		this.largeurImage = this.img.getWidth();
-		this.hauteurImage = this.img.getHeight();
-		
+		this.hauteurImage = this.img.getHeight();	
 	}
 	
+	/**
+	 * @author Clément
+	 * Getteur qui retourne la photo 
+	 * @return
+	 */
     public BufferedImage getImage() {
         return this.img;
     }
     
+    /**
+     * @author Clément 
+     * Procédure permettant de convertir la matrice en niveau de gris 
+     */
     public void convertirEnNiveauDeGris() {
         for (int y = 0; y < this.hauteurImage; y++) {
             for (int x = 0; x < this.largeurImage; x++) {
@@ -42,6 +58,9 @@ public class Image {
         }
     }
     
+    /**
+     * Procédure permettant de vectoriser la matrice (transformation en vecteur colonne)
+     */
     public void vectoriser() {
     	int cpt = 0;
     	double[] tab = new double[this.hauteurImage * this.largeurImage];
@@ -53,4 +72,24 @@ public class Image {
     	}
     	this.vecteurImage = new Vecteur(tab);
     }
+    
+    @Override
+    /**
+     * Retourne le chemin de l'image sous forme de chaine de caractere
+     * @return (String)
+     */
+    public String toString() {
+    	return this.id;
+    }
+    
+    
+   /**
+    * Retourne le vecteur colonne associé à l'image
+    * @return (Vecteur) 
+    */
+    public Vecteur getVecteurImage() {
+    	return this.vecteurImage;
+    }
+    
+    
 }
