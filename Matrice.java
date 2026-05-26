2ede0887f52dde6a1b348ca9d3b987fcf5a08e04
e-dcazenav <dorian.cazenave@etu.cyu.fr>
2026-05-26 10:30:37 +0200

    debut matrice

--- Matrice.java
+++ Matrice.java
@@ -1,1 +1,37 @@
+public class Matrice{
+	
+	private Vecteur[] A; // matrice
+	private int n,m;
+	
+	public Matrice(double[][] a) {
+		A = a;
+		n=100;
+		m=100;
+	}
+	
+	public double[][] getA() {
+		return A;
+	}
 
+	public void setA(double[][] a) {
+		A = a;
+	}
+	
+	public double[][] calculTransposee(Vecteur[] A) {
+		/*
+		 * Fonction calculant la transposée de A
+		 * @author Dorian
+		 * @param A tableau 2D
+		 * @return B tableau 2D transposée de A
+		 */
+	    double Vecteur[] B = null;
+
+	    for (int i = 0; i < A.length; i++) {
+	        for (int j = i + 1; j < A.length; j++) {
+	            B[i][j] = A[j][i];
+	            B[j][i] = A[i][j];
+	        }
+	    }   
+	    return B;
+	}
+}
