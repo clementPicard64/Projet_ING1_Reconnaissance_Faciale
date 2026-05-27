f819ee7b8617c549a1d10f3dc3567292e1c3bd43
celiacandy <candycelia@cy-tech.fr>
2026-05-27 15:17:48 +0200

    Implement dot product method in Vecteur class
    Added a method to compute the dot product of two vectors.

--- Vecteur.java
+++ Vecteur.java
@@ -30,4 +30,18 @@ public class Vecteur {
 	public void setVecteur(double[] vecteur) {
 		this.vecteur = vecteur;
 	}
+
+		/**
+	 * Description : prends deux vect et en fait le produit scalaire
+	 * @author CandyCelia
+	 * @param autre un vecteur
+	 * @return somme un double
+	 */
+	public double produitScalaire(Vecteur autre) {
+	    double somme = 0;
+	    for (int i = 0; i < this.vecteur.length; i++) {
+	        somme += this.vecteur[i] * autre.vecteur[i];
+	    }
+	    return somme;
+	}
 }
