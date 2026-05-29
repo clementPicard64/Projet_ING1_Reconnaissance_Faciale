d8f99d86f27f61058f9a376da42ca162dc0385d1
Yassine1049 <yassinocegb@gmail.com>
2026-05-29 11:38:39 +0200

    Update Matrice.java

--- Matrice.java
+++ Matrice.java
@@ -1,4 +1,4 @@
-
+import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.SingularValueDecomposition;
