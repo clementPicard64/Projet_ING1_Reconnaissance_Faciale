3e54791c22e47201bf9c20b720c0265ad067a0ae
celiacandy <candycelia@cy-tech.fr>
2026-06-01 22:35:53 +0200

    Create Client class for user interaction management

---
+++ Client.java
@@ -0,0 +1,21 @@
+package projet;
+
+/**
+ * Description : gère les interactions avec l'utilisateur
+ * @author CandyCelia
+ * @version 1.0
+ */
+public class Client {
+    /**
+     * creer un bouton pour ajouter une personne
+     */
+	public void boutonAjouterPersonne() {  // A FAIRE
+	}
+	
+    /**
+     * creer un bouton pour lancer l'identification
+     */
+	public void boutonLancerIdentification() {  // A FAIRE
+	}
+	
+}
